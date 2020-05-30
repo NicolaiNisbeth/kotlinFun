7d0e3d1e1b2715762a5affc6a4ee613bcbbd843d
NicolaiNisbeth <s175565@student.dtu.dk>
2020-05-30 13:34:47 +0200

    initial commit for single activity architecture

--- app/src/main/java/com/example/kotlineksperiments/MainActivity.kt
+++ app/src/main/java/com/example/kotlineksperiments/MainActivity.kt
@@ -12,26 +12,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val data = generateData(10)
-        val adapter = ExampleAdapter(data)
-        recycler_view.adapter = adapter
-        recycler_view.layoutManager = LinearLayoutManager(this)
-        recycler_view.setHasFixedSize(true)
-
-        adapter.setOnItemClickListener(object : ExampleAdapter.OnItemClickListener{
-            val sheet = BottomSheet()
-            override fun onItemClick(item: ExampleItem) {
-                sheet.item = item
-                sheet.show(supportFragmentManager, "dialog")
-            }
-        })
     }
 
-    private fun generateData(size: Int): List<ExampleItem> {
-        val list = ArrayList<ExampleItem>()
-        for (i in 0 until size){
-            list += ExampleItem("Headline $i", "Subtile $i")
-        }
-        return list
-    }
+
 }
